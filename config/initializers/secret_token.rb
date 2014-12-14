3497ff93dfff852991c4c752a90a359cbd0be25a
miho <hayashimihoo@gmail.com>
2014-12-13 21:17:37 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+More1214::Application.config.secret_key_base = 'abd63c2e9afc1e9fffc4be100000d99049154f352f0d0d504c0dc0756f9381db4129e73bf65f40af4c59d43636cedf957c305cc783998fce87673c4010d16e71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+More1214::Application.config.session_store :cookie_store, key: '_more1214_session'
